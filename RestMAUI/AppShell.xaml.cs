1f0e9810e0d59bf7d500bf110d4477b6f7f69180
Steven Lizarzaburu Pezúa <lizarzaburupezuasteven@gmail.com>
2023-04-25 00:02:36 -0500

    Creacion del Proyecto

---
+++ RestMAUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace RestMAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ RestMAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace RestMAUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
